328aeb8d66446febe0d69adb6009e867b4eb8e11
Ekaterina <pechnikova.07@mail.ru>
2022-06-18 21:49:20 +0300

    Cubes Table Of N

---
+++ Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using System; 
+using static System.Console; 
+
+Clear();
+
+Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= number)
+{
+    
+    WriteLine($"{i} * {i} * {i} = {Math.Pow(i, 3)}"); // вывод таблицы
+    // Write($"{Math.Pow(i, 3)} ");                   // вывод в строку
+    i++;
+}
+WriteLine();
+
